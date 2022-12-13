ad05774dec73e0fb7c5dfa15053c737573fbcccb
Tuba GÜMÜS ESMEK <119704176+tubagumus@users.noreply.github.com>
2022-12-13 13:15:06 +0100

    add changes 04-Tuba

---
+++ WEEK-1 VARIABLES/04-Tuba/CourseScoreCalculation.py
@@ -0,0 +1,46 @@
+Name :   input("Vorame des Studenten:")
+Nahname : input ("Nachname des Studenten:")
+Nummer :  input("Studentnummer:")
+
+Visa = input('MatheVisa : ')
+Final= input('MatheFinal: ')
+durchschnitt= (float((int(Visa)*40)/100) + float((int(Final)*60)/100))
+print("Durchschnitt:", durchschnitt)
+
+if (durchschnitt) >= 50:
+   print("PASSED")
+elif (durchschnitt) <50:
+    print("ERFOLGLOS")
+
+
+Visa = input('BiologieVisa : ')
+Final= input('BiologieFinal: ')
+urchschnitt= (float((int(Visa)*40)/100) + float((int(Final)*60)/100))
+print("Durchschnitt:", durchschnitt)
+
+if (durchschnitt) >= 50:
+   print("PASSED")
+elif (durchschnitt) <50:
+    print("ERFOLGLOS")
+
+
+Visa = input('PhysikVisa : ')
+Final= input('PhysikFinal: ')
+urchschnitt= (float((int(Visa)*40)/100) + float((int(Final)*60)/100))
+print("Durchschnitt:", durchschnitt)
+
+if (durchschnitt) >= 50:
+   print("PASSED")
+elif (durchschnitt) <50:
+    print("ERFOLGLOS")
+
+
+Visa = input('DeutschVisa : ')
+Final= input('DeutschFinal: ')
+durchschnitt= (float((int(Visa)*40)/100) + float((int(Final)*60)/100))
+print("Durchschnitt:", durchschnitt)
+
+if (durchschnitt) >= 50:
+   print("PASSED")
+elif (durchschnitt) <50:
+    print("ERFOLGLOS")
