aea57d13f73beaed4317f60d20684772117a04d2
Adem DOĞAN <119605507+Ademdogan2021@users.noreply.github.com>
2022-12-16 20:49:16 +0100

    Delete WEEK-2 LIST DICTIONARY TUPLE SET/12- Adem directory

--- WEEK-2 LIST DICTIONARY TUPLE SET/12- Adem/Word_Counts.py
+++
@@ -1,9 +0,0 @@
-#Calculation of letter numbers of the entered sentence
-a = str( input("Enter the sentence")).lower()
-print (a)
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-#Search each letter within the alphabet 
-for i in alphabet: 
-#For not print letters that are not in the entered sentence and take the value "0"
-    if a.count(i) > 0:
-        print (tuple((i  , a.count(i))))
