4c455e88ae7858b84dd9f7c8a166ff26166dbed6
Alper Arslan <arslanalper88@gmail.com>
2022-12-19 01:34:09 +0100

    comments have been added

--- WEEK-2 LIST DICTIONARY TUPLE SET/11-Musab-Sari/set_of_two_words.py
+++ WEEK-2 LIST DICTIONARY TUPLE SET/11-Musab-Sari/set_of_two_words.py
@@ -4,18 +4,25 @@ Date        : 13.12.2022
 
 Application : Set of Two Words
 
-Explanation : Set of two words is an application where user inputs two words and outputs in a list as follows shared letters of the words, unique letters of word one
+Explanation : Set of two words is an application where user inputs two words and outputs in a list 
+as follows shared letters of the words, unique letters of word one
 and unique letters of word two.
 '''
 
-list_of_outputs = [] #list created to be able to see desired output
-word_1 = set(input('Word 1:')) #user input for word1
-word_2 = set(input('Word 2:')) #user input for word2
+list_of_outputs = []  # list created to be able to see desired output
+word_1 = set(input('Word 1:'))  # user input for word1
+word_2 = set(input('Word 2:'))  # user input for word2
 
-shared_letters  = (''.join(sorted(list(word_1&word_2)))) #functions of the application
-diffirence1 = (''.join(sorted(list(word_1-word_2))))
-diffirence2 = (''.join(sorted(list(word_2-word_1))))
+shared_letters  = (''.join(sorted(list(word_1 & word_2))))
+diffirence1 = (''.join(sorted(list(word_1 - word_2))))
+diffirence2 = (''.join(sorted(list(word_2 - word_1))))
+# difference() ve intersection() set metodlarini da kullanabiliriz.
 
-list_of_outputs[0:3] = shared_letters, diffirence1, diffirence2 #output block
+list_of_outputs[0:3] = shared_letters, diffirence1, diffirence2  # output block
+# outputs = [shared_letters, diffirence1, diffirence2]  
+# bu sekilde de farkli turdeki veri yapilarini direk listenin icine gomebilirsin
+print(list_of_outputs)
 
-print(list_of_outputs) 
+''' 
+Cok guzel, tebrikler. 
+'''
