e7c0c635a591ebfc265833faeffa546f8a2af124
alicamoglu <alicam0121@gmail.com>
2022-12-23 15:28:11 +0100

    Update Body_Mass_Index.py

--- WEEK-1 VARIABLES/05-Nuseybe/Body_Mass_Index.py
+++ WEEK-1 VARIABLES/05-Nuseybe/Body_Mass_Index.py
@@ -2,7 +2,7 @@ user_height = input ("Please enter your height :")
 us_height=float (user_height)
 user_weight = input ("Please input your weight :")
 us_weight=float (user_weight)
-Bmi= (us_weight/((us_height/100)*(us_height/100)))
+Bmi= (us_weight/((us_height/100)*(us_height/100))) #Bmi(Body_Mass_Index)
 if Bmi<=20.5 and  Bmi >=18.5 :
     print (f"Your Bady Mass Index {Bmi:.2f}, Normal")
 elif Bmi>25 and Bmi<=30 :
