538271b99457185033c933e291506af4b58cfdad
Tuba GÜMÜS ESMEK <119704176+tubagumus@users.noreply.github.com>
2022-12-25 22:25:30 +0100

    add changes 04-TubaG

--- WEEK-3 FUNCTIONS/04-TubaG/readingnumber.py
+++ WEEK-3 FUNCTIONS/04-TubaG/readingnumber.py
@@ -11,15 +11,19 @@
 
 def reading_number (n):
 
-    ones_digit = ["one", "two","three", "four","five", "six","seven","eight", "nine", ] 
+    ones_digit = [" ","one", "two","three", "four","five", "six","seven","eight", "nine", ] 
     #write  the numbers in the ones digit as a list
-    tens_digit = ["ten", "twenty", "thirty","forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    tens_digit = [" ","ten","eleven","twenty", "thirty","forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
     ##write  the numbers in the tens digit as a list
-
+    more_than_digit = [" ", "eleven", "twelve", "thirteen", "fourteen", "fifteen","sixteen","seventeen","nineteen"]
     ones = n%10   #returns the reamining  from division of 10
     tens = n// 10 #the division gives the tens digit
+    if (n//10==1):
+        print(f"\n{str(n)} ----------------> {more_than_digit[n%10]}")
+
+    else:
     
-    print(tens_digit[tens], ones_digit[ones]) #creating and printing  digits
+       print(tens_digit[tens], ones_digit[ones]) #creating and printing  digits
 
 new_number= int(input("write a number:\n"))
 reading_number(new_number)
