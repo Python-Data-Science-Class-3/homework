67abfc4dcb1d6e311043e1e3b767b3e96db21795
Tayyip Güney <84155765+tguneyt@users.noreply.github.com>
2022-12-26 12:18:07 +0100

    Update egualreverse.py

--- WEEK-3 FUNCTIONS/04-TubaG/egualreverse.py
+++ WEEK-3 FUNCTIONS/04-TubaG/egualreverse.py
@@ -16,20 +16,30 @@ Subject   : Equal Reverse
 
 def equal_reverse(word_list):  #creatin the function
     
-    word_list =list(reversed(word_list))  #sort parameter in reverse order to create a List
+    # word_list =list(reversed(word_list))  #sort parameter in reverse order to create a List
+    '''reverse etmemize gerek yok. input ile aldigimiz listeyi degistiriyor ve yanlis sonuca sebep oluyor'''
+    
+    result_list = [] 
+    '''her bir kelime icin kontrol etmeliyiz bunlarida bir listede tutalim.'''
     
     for i in word_list:    #returning  the parameter with a for loop
         if i == i[::-1]: #if the parameter is equal in rerverse order
-         return True #True command
+            
+            result_list.append(True) 
+            '''her seferinde return etmek yerine listede tutalim'''
+        #  return True #True command
+        
+        else:  #if the parameter is not equal in reverse, False command
 
-    else:
-         return False
- #if the parameter is not equal in reverse, False command
-         
-    
+            result_list.append(False)
+        #  return False
+        
+    '''Sonuc olarak listeyi return edebiliriz'''
+    return result_list 
 
 #palindrom words =["racecar","level", "reviver", "redder"] #inverse equal words
 #other words =["cheap","expensive", "home"]  #inverse unequal words
 
 words = (list(input("write a palindrom word:\n ").split('-')))
+
 print(equal_reverse(words))
