b26b5e4acdd1286bc643a81166ba424671b1ccc3
Tayyip Güney <84155765+tguneyt@users.noreply.github.com>
2022-12-26 12:14:36 +0100

    Update perfectnumber.py

--- WEEK-3 FUNCTIONS/04-TubaG/perfectnumber.py
+++ WEEK-3 FUNCTIONS/04-TubaG/perfectnumber.py
@@ -10,24 +10,38 @@
 # #Check perfect numbers between 1 and 1000 and find the sum of the perfect numbers using reduce and filter functions.
 
 
+
 from functools import  reduce
 
-def diving_numbers(num):
-    divisors = []
-
-    sum = 0
-    for i in range(1, num):
-        if (num % i== 0):
-            sum= sum+ i
-            divisors.append(i)   #i +i/n
-
-    return divisors
-
-#print(diving_numbers())
+def diving_numbers(num):  #creating the function
+    sum=0                 
+    for i in range(1,num): 
+        if num%i == 0:        
+            sum=sum+i       
+    if sum==num:         # eger perfect number ise return yap    
+        return sum
 
 
 
-a=diving_numbers (1000) 
-#end  = reduce(filter(lambda x:+= sum//, a))
-end = reduce(lambda b,c : b+c , a)
-print(a)
+# res = reduce(lambda x,y : x+y, )
+# print(diving_numbers(1000)) 
+
+div_list = list(filter(diving_numbers,range(1,1001)))
+'''
+bu fonksiyona 1000 kere gidip return olarak perfect number i filter ile alip listeye ceviriyor.
+elimizde perfect numberlardan olusan bir liste var.
+'''
+print(f"{reduce(lambda x,y: x+y,div_list)}") # elimnizde div_list var ve icindeki degerleri reduce ile toplayalim.
+
+# print(f"{reduce(lambda x,y: x+y,list(filter(diving_numbers,range(1,1001))))}") # birlestirip tek satirda yazdik
+
+
+'''
+Ornek reduce()
+
+from functools import reduce
+sayilar = [1, 3, 5, 7, 9]
+toplam = reduce (lambda x, y : x +y, sayilar )
+print( toplam )
+
+'''    
