4e3a4f06b9354c49b6db59103ae32995e2b4a0af
Tayyip Güney <84155765+tguneyt@users.noreply.github.com>
2022-12-26 12:23:03 +0100

    Update readingnumber.py

--- WEEK-3 FUNCTIONS/04-TubaG/readingnumber.py
+++ WEEK-3 FUNCTIONS/04-TubaG/readingnumber.py
@@ -23,7 +23,12 @@ def reading_number (n):
 
     else:
     
-       print(tens_digit[tens+1], ones_digit[ones]) #creating and printing  digits
+       print(tens_digit[tens], ones_digit[ones]) #creating and printing  digits
+       ''' 
+       print(tens_digit[tens+1], ones_digit[ones])
+        
+       tens_digit[tens+1] olmali. Yanlis sonucu veriyor. Calistirip kontrol edin.
+       '''
 
 new_number= int(input("write a number:\n"))
 reading_number(new_number)
