12a681764129bcf2e1e0261f701810d433baf974
yunus-dev <84150928+yunusdonmez-dev@users.noreply.github.com>
2022-12-29 09:31:33 +0100

    add_changes_01_Yunus

---
+++ WEEK-4 MODULES/01-Yunus/calculator/division.py
@@ -0,0 +1,3 @@
+from math import ceil
+def division(x,y):
+    print(f"{x}/{y} ------> {ceil(x/y)}") 
---
+++ WEEK-4 MODULES/01-Yunus/calculator/subtraction.py
@@ -0,0 +1,5 @@
+from math import ceil
+
+
+def subtraction(x,y):
+    print(f"{x}-{y} ------> {ceil(x-y)}") 
