727ad6651ec7a0889563b627e7c170ace3217246
Alper Arslan <arslanalper88@gmail.com>
2023-01-08 23:11:15 +0100

    add changes Alper-Arslan

--- WEEK-5 OOP/06-Sümeyra/product_info.py
+++ WEEK-5 OOP/06-Sümeyra/product_info.py
@@ -7,7 +7,7 @@
 #A decimal to be calculated as (product_sale_price - product_purchase_price)
 #Remarks - To store "Profit" if Margin is positive else "Loss" if Margin is negative.
 #Methods :
-#A constructor to intialize all the data members with valid default values.
+#A constructor to intialize all the data membe1rs with valid default values.
 #A method setRemarks() that assigns Margin as (product_sale_price - product_purchase_price)and sets Remarks as mentioned below :
 #Margin	Remarks <0 (negative)	Loss >0 (positive)	Profit
 #A method set_details() to accept values for product_id. product_name, product_purchase_price, product_sale_price and invokes SetRemarks() method.
@@ -15,14 +15,22 @@
 
 
 class Product:
-    def __init__(self,product_id = '',product_name = '',product_purchase_price = 0,product_sale_price = 0):
+    def __init__(self,product_id = '', product_name = '', product_purchase_price = 0, product_sale_price = 0):
       self.product_id = product_id
       self.product_name = product_name
-      self.product_purchase_price = product_purchase_price 
+      self.product_purchase_price = product_purchase_price
       self.product_sale_price = product_sale_price
+      ### good initial values, you can try to choose float number for prices (0.0)
+      ### type(0) ---> int /// type(0.0) ---> float
       
       
-    def setRemarks(self):          #A method that assigns Margin as (product_sale_price - product_purchase_price)
+    def setRemarks(self):          # A method that assigns Margin as (product_sale_price - product_purchase_price)
+        ### set_remarks !!!
+
+        ### Where did you create self.remarks?
+        ### There is no self.remarks object in the class.
+        ### I have seen it in the following function, but if you won't call set_details but only setRemarks,
+        ### then it's 
         if self.remarks < 0 :
              return "Loss"
         elif self.remarks > 0 : 
@@ -30,7 +38,7 @@ class Product:
         else:
             print("Neither Profit Nor Loss")     
 
-    def set_details(self):          #A method set_details() to accept values for data members
+    def set_details(self):          # A method set_details() to accept values for data members
         self.product_id = input("Enter product id: ")     
         self.product_name = input("Enter product name: ")
         self.product_purchase_price = float(input("Enter product purchase price: "))
@@ -45,8 +53,37 @@ class Product:
 product = Product()
 product.set_details()
 print(product.get_details())
-            
+
+### You can take a look at the following example
+
+"""
+class Product:
+
+    def __init__(self, product_id = None, product_name = None, product_purchase_price = 0.0, product_sale_price = 0.0):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.product_purchase_price = product_purchase_price
+        self.product_sale_price = product_sale_price
+
+    def set_remarks(self):
+        self.margin = self.product_sale_price - self.product_purchase_price
+        
+        if self.margin > 0:
+            print("Profit")
+        
+        elif self.margin < 0:
+            print("Loss")
+
+        else:
+            print("Neither Profit Nor Loss")
 
 
+
+product_1 = Product(product_purchase_price = 35.5, product_sale_price = 45)
+product_1.set_remarks()
+
+product_2 = Product(product_purchase_price = 45, product_sale_price = 45)
+product_2.set_remarks()
+"""
 
 
--- WEEK-5 OOP/12- Adem/Item_info.py
+++ WEEK-5 OOP/12- Adem/Item_info.py
@@ -32,22 +32,27 @@ class Iteminfo:
         if self.qty <= 10:
             self.discount = 10
 
-        if self.qty >= 11 or self.qty<=28 :
+        if self.qty >= 11 or self.qty<=28 :  ### I think it should be "< 20" 
             self.discount = 15
 
         if self.qty >=20:
             self.discount =20
 
+        ### We should calculate the net price in here, namely in this function
+
     #Entering information and calculating the price
+    ### We need them from the user with user inputs
     def buy(self,item_code,item,price,qty):
         self.item_code = item_code
         self.item = item
         self.price = price
         self.qty = qty
         self.calculate_discount()
-        self.net_price = self.price*self.qty -self.discount
-        
-    #Show all content
+        self.net_price = self.price*self.qty -self.discount  ### This line should be in the calculate_discount function
+        ### Then you can call also it with self.calculate_discount() line.
+
+    # Show all content
+    ### We can show all of them with one print function
     def show_all(self):
         print(f"Item code:", self.item_code)
         print(f"Item :", self.item)
@@ -60,3 +65,67 @@ item = Iteminfo()
 #Calling the function by entering the values of item_code,item,price,qty
 item.buy(12, "Iphone 13", 20, 15)
 item.show_all()
+
+
+### You can take a look at the following solution from me
+
+"""
+class ItemInfo:
+
+    def __init__(self, item_code = 0, item = None, price = 0, qty = 0, discount = 0, net_price = 0):
+        self.item_code = item_code
+        self.item = item
+        self.price = price
+        self.qty = qty
+        self.discount = discount
+        self.net_price = net_price
+
+    def calculate_discount(self):
+        if self.qty <= 10:
+            self.discount = 0
+
+        elif 11 <= self.qty < 20:
+            self.discount = 15
+
+        elif self.qty >= 20:
+            self.discount = 20
+
+        self.net_price = self.price * self.qty - self.discount
+
+    def buy(self):
+        self.item_code = input("Type the code of the item: ")
+        self.item = input("Type the name of the item: ")
+        self.price = int(input("Type the price of the item: "))
+        self.qty = int(input("Type the quantity of the item: "))
+
+        self.calculate_discount()
+
+    def show_all(self):
+        return {
+                "item_code":self.item_code, 
+                "item_name":self.item, 
+                "item_price":self.price, 
+                "item_quantity":self.qty, 
+                "discount_amount":self.discount, 
+                "net_price":self.net_price
+                }
+
+
+all_items = []  # for creating a list to show all items which will have been created with inputs
+
+finished = False
+
+while not finished:  
+
+    item = ItemInfo()
+    item.buy()
+    print(item.show_all())
+    all_items.append(item.show_all())
+
+    inp = input("Will you want to add new item, then you can go ahead with typing any button then Enter. \nIf you want to stop, you can type just 'N'")
+
+    if inp == "N":
+        finished = True
+
+print(all_items)
+"""
