e60e090759017d58996448cae2a75631c793ff8f
Ebubekir Baktır <ebubekirbaktir@users.noreply.github.com>
2023-01-19 09:32:10 +0100

    add cahanges 08_ebubekir

---
+++ WEEK-7 FILE_IO_REGEX/08_Ebubekir/03_find_emails_in_text.py
@@ -0,0 +1,22 @@
+"""Question 3:
+Write a program that list according to email addresses without email domains in a text.
+
+Example:
+
+Input:
+
+The advencements in biomarine studies franky@google.com with the investments necessary and Davos sinatra123@yahoo.com.
+ Then New Yorker article on wind farms...
+
+Output :
+
+franky
+
+sinatra123"""
+
+import re
+input="The advencements in biomarine studies franky@google.com with the investments necessary and Davos sinatra123@yahoo.com. Then New Yorker article on wind farms..."
+
+pattern=r"(\w{1,}|\d)@"
+a=re.findall(pattern,input)
+print(a)
