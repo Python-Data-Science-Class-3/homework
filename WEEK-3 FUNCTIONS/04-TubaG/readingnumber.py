7e82c90c8b35f651beb595e7dbc44d18d97eda87
Tuba GÜMÜS ESMEK <119704176+tubagumus@users.noreply.github.com>
2023-10-28 20:27:17 +0200

    add changes tuba

--- WEEK-3 FUNCTIONS/04-TubaG/readingnumber.py
+++ WEEK-3 FUNCTIONS/04-TubaG/readingnumber.py
@@ -11,24 +11,36 @@
 
 def reading_number (n):
 
-    ones_digit = [" ","one", "two","three", "four","five", "six","seven","eight", "nine", ] 
+    ones_digit = ["","one", "two","three", "four","five", "six","seven","eight", "nine", ] 
     #write  the numbers in the ones digit as a list
-    tens_digit = [" ","ten","eleven","twenty", "thirty","forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    tens_digit = ["","ten","eleven","twenty", "thirty","forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
     ##write  the numbers in the tens digit as a list
-    more_than_digit = [" ", "eleven", "twelve", "thirteen", "fourteen", "fifteen","sixteen","seventeen","nineteen"]
+    more_than_digit = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen","sixteen","seventeen","nineteen", ""]
     ones = n%10   #returns the reamining  from division of 10
     tens = n// 10 #the division gives the tens digit
-    if (n//10==1):
-        print(f"\n{str(n)} ----------------> {more_than_digit[n%10]}")
 
+
+    if 10 < n < 20:
+        print(f"{n} ----------------> {more_than_digit[n - 10]}")
     else:
-    
-       print(tens_digit[tens], ones_digit[ones]) #creating and printing  digits
-       ''' 
-       print(tens_digit[tens+1], ones_digit[ones])
-        
-       tens_digit[tens+1] olmali. Yanlis sonucu veriyor. Calistirip kontrol edin.
-       '''
+        print(f"{tens_digit[tens]} {ones_digit[ones]}")
 
-new_number= int(input("write a number:\n"))
+new_number = int(input("write a number:\n"))
 reading_number(new_number)
+
+
+
+#     if (n//10==1):
+#         print(f"\n{str(n)} ----------------> {more_than_digit[n%10]}")
+
+#     else:
+    
+#        print(tens_digit[tens], ones_digit[ones]) #creating and printing  digits
+#        ''' 
+#        print(tens_digit[tens+1], ones_digit[ones])
+        
+#        tens_digit[tens+1] olmali. Yanlis sonucu veriyor. Calistirip kontrol edin.
+#        '''
+
+# new_number= int(input("write a number:\n"))
+# reading_number(new_number)
